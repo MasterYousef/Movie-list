ad6e6575e65f2c279131a0455cfd97ea76d9c4aa
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-02-14 17:35:42 +0200

    updates packages

--- src/components/Pagenum.js
+++ src/components/Pagenum.js
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import ReactPaginate from 'react-paginate';
-import { useDispatch } from 'react-redux';
-import { GetPage } from '../redux/actions/movieAction';
+import React from "react";
+import ReactPaginate from "react-paginate";
+import { useDispatch } from "react-redux";
+import { GetPage } from "../redux/actions/movieAction";
 function Pagenum() {
-  const dispatch = useDispatch()
-let handlePageClick =(data)=>{
-  dispatch(GetPage(data.selected +1))
-    };
+  const dispatch = useDispatch();
+  let handlePageClick = (data) => {
+    dispatch(GetPage(data.selected + 1));
+  };
   return (
-    <>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="التالي"
-        onPageChange={handlePageClick}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={3}
-        pageCount={500}
-        previousLabel="السابق"
-        renderOnZeroPageCount={null}
-        containerClassName='pagination justify-content-center p-3 ul'
-        pageClassName='page-item'
-        pageLinkClassName='page-link'
-        previousClassName='page-item'
-        previousLinkClassName='page-link'
-        nextClassName='page-item'
-        nextLinkClassName='page-link'
-        breakClassName='page-link'
-        activeClassName='active'
-      />
-    </>
-  )
+    <ReactPaginate
+      breakLabel="..."
+      nextLabel="التالي"
+      onPageChange={handlePageClick}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={3}
+      pageCount={500}
+      previousLabel="السابق"
+      renderOnZeroPageCount={null}
+      containerClassName="pagination justify-content-center p-3 ul"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      previousClassName="page-item"
+      previousLinkClassName="page-link"
+      nextClassName="page-item"
+      nextLinkClassName="page-link"
+      breakClassName="page-link"
+      activeClassName="active"
+    />
+  );
 }
-export default Pagenum
+export default Pagenum;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,8 @@
-import { createStore , applyMiddleware,compose } from 'redux'
-import { moviesReducer } from '../reducer/movieReducer'
-import {composeWithDevTools} from "redux-devtools-extension"
-import thunk from 'redux-thunk'
-const redfun = compose(applyMiddleware(thunk),composeWithDevTools())
+import { createStore, applyMiddleware, compose } from "redux";
+import { moviesReducer } from "../reducer/movieReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+const redfun = compose(applyMiddleware(thunk), composeWithDevTools());
 
 //1-create store
-export const store = createStore(moviesReducer,redfun);
+export const store = createStore(moviesReducer, redfun);
