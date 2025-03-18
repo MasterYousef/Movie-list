dd34ec882b5802c9e073d8bc620b5b8a32d60dca
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-03-18 03:06:30 +0200

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { moviesReducer } from "../reducer/movieReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
-const redfun = compose(applyMiddleware(thunk), composeWithDevTools());
+const redfun = compose(applyMiddleware(thunk));
 
 //1-create store
 export const store = createStore(moviesReducer, redfun);
